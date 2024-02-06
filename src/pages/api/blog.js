38f8566fb06b365d124e591b46d05463807d66d8
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-02-06 11:38:37 +0800

    AQWS

--- src/pages/api/blog.js
+++ src/pages/api/blog.js
@@ -1,6 +1,16 @@
 export default async function handler(request, response) {
-  const data = await fetch("https://dev.to/api/articles/150589");
-  const blog = await data.json();
+  const highlight = await fetch("https://dev.to/api/articles?top=1&per_page=5");
+  const hlData = await highlight.json();
+
+  // const trend = await fetch("https://dev.to/api/articles?top=1&per_page=4");
+  // const trendData = await trend.json();
+
+  // const post = await fetch("https://dev.to/api/articles?top=30&per_page=9");
+  // const postData = await post.json();
+
   response.setHeader("Access-Control-Allow-Origin", "*");
-  response.status(200).json(blog);
+
+  response.status(200).json(hlData);
+  // response.status(200).json(trendData);
+  // response.status(200).json(postData);
 }
--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { randomUsers } from "@/utils/dummyUsers";
+// export default async function handler(request, response) {
+//   const post = await fetch("https://dev.to/api/articles?top=30&per_page=9");
+//   const postData = await post.json();
 
-export default function handler(request, response) {
-  response.status(200).json(randomUsers);
-}
+//   response.setHeader("Access-Control-Allow-Origin", "*");
+
+//   response.status(200).json(postData);
+// }
