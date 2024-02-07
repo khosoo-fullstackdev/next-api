17db0ed6cb0b715be46842aefbcec6287aac3e1c
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-02-07 10:06:40 +0800

    backEnd

---
+++ src/pages/api/post.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(request, response) {
+  const post = await fetch("https://dev.to/api/articles?top=30&per_page=9");
+  const postData = await post.json();
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  response.status(200).json(postData);
+}
---
+++ src/pages/api/trend.js
@@ -0,0 +1,10 @@
+// API ROUTE, ENDPOINT,
+
+export default async function handler(request, response) {
+  const trend = await fetch("https://dev.to/api/articles?top=1&per_page=4");
+  const trendData = await trend.json();
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  response.status(200).json(trendData);
+}
